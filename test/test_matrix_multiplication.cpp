4ad629eeabd808a85fa3621d1529acf4ef72cf3a
martinaraffaelli <martina.raffaelli@mail.polimi.it>
2024-05-31 10:23:26 +0200

    updated test cases

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -6,18 +6,20 @@
 #include <random>
 #include <cmath>
 #include <climits>
-#include <cstdlib> // for std::rand
+#include <cstdlib>
 #include "../src/matrix_mult.cpp"
 
 const int N_max = 10;
 const int N_min = 1;
+const int std_length = 20;
+const int std_lower = 10;
 // ######################### Source code of multiplyMatrices in src/matrix_mult
 
-// Fills the matrix with random values of the standard interval [-5, 10]
+// Fills the matrix with random values of the standard interval [-std_lower, std_length - std_lower]
 void fillMatrixRandomly(std::vector<std::vector<int>>& A) {
     for (auto& row : A) {
         for (auto& element : row) {
-            element = (std::rand() % 15) - 5; // Generate random number between 0 and 99
+            element = (std::rand() % std_length) - std_lower; // Generate random number between -std_lower, std_length - std_lower
         }
     }
 }
@@ -46,15 +48,15 @@ int generateRandomNumber(int lowerLimit, int upperLimit) {
 }
 
 
-//StandardMatrixMultiplicationTest is the suite test that groups the tests with random
-//matrix generation
-
-//The tests that are included in this suite differ in the types of matrices that are created,
-//in particular we considered: 
+// StandardMatrixMultiplicationTest is the suite test that groups the tests with random matrix generation
+// The tests that are included in this suite differ in the types and sizes of matrices that are created,
+// in particular we considered: 
 // - both rectangular matrices
 // - both square matrices
 // - a rectangular matrix and a square one
 // - a square matrix and a rectangular one
+// - both square matrices with intermediate dimensions
+// - both square matrices with big dimensions
 
 TEST(StandardMatrixMultiplicationTest, RectangularMatrices) {
 
@@ -186,18 +188,89 @@ TEST(StandardMatrixMultiplicationTest, SquareandRectangularMatrices) {
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
+TEST(StandardMatrixMultiplicationTest, IntermediateRectangularMatrices) {
 
-//ExtremeMatrixMultiplicationTest is the suite test that groups the specific cases we want to test,
-//such as:
+    // Randomly generate the dimensions of A
+    int rowsA = generateRandomNumber(11, 20);
+    int colsA = generateRandomNumber(11, 20);
+
+    // Create A matrix 
+    std::vector<std::vector<int>> A(rowsA, std::vector<int>(colsA));
+
+    // Fill A
+    fillMatrixRandomly(A);
+    
+    // Set proper dim for B
+    int rowsB = colsA;
+    int colsB = generateRandomNumber(11, 20);
+
+    // Create B matrix 
+    std::vector<std::vector<int>> B(rowsB, std::vector<int>(colsB));
+
+    // Fill B
+    fillMatrixRandomly(B);
+
+    std::vector<std::vector<int>> C(rowsA, std::vector<int>(colsB, 0));
+
+    multiplyMatrices(A, B, C, rowsA, colsA, colsB);
+
+    std::vector<std::vector<int>> expected(rowsA, std::vector<int>(colsB, 0));
+    multiplyMatricesWithoutErrors(A,B,expected,rowsA,colsA,colsB);
+
+    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
+}
+
+TEST(StandardMatrixMultiplicationTest, BigRectangularMatrices) {
+
+    // Randomly generate the dimensions of A
+    int rowsA = generateRandomNumber(21, 100);
+    int colsA = generateRandomNumber(21, 100);
+
+    // Create A matrix 
+    std::vector<std::vector<int>> A(rowsA, std::vector<int>(colsA));
+
+    // Fill A
+    fillMatrixRandomly(A);
+    
+    // Set proper dim for B
+    int rowsB = colsA;
+    int colsB = generateRandomNumber(21, 100);
+
+    // Create B matrix 
+    std::vector<std::vector<int>> B(rowsB, std::vector<int>(colsB));
+
+    // Fill B
+    fillMatrixRandomly(B);
+
+    std::vector<std::vector<int>> C(rowsA, std::vector<int>(colsB, 0));
+
+    multiplyMatrices(A, B, C, rowsA, colsA, colsB);
+
+    std::vector<std::vector<int>> expected(rowsA, std::vector<int>(colsB, 0));
+    multiplyMatricesWithoutErrors(A,B,expected,rowsA,colsA,colsB);
+
+    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
+}
+
+
+
+// ExtremeMatrixMultiplicationTest is the suite test that groups the specific cases we want to test,
+// such as specific numbers, specific structures and specific dimensions,
+// in particular we considered:
 // - vectors 
 // - matrix and vector 
 // - vector and matrix 
 // - scalars 
+// - scalar and vector
+// - matrices with odd numbers of rows and cols
+// - matrices with even numbers of rows and cols
 // - identity matrices 
-// - odd and even dimensions 
-// - null matrices 
-// - nan matrices 
-// - overflow
+// - square matrices with all values equal to 0 
+// - square matrices with all values equal to 1
+// - ractangular matrices with small values (from 0 to 10) 
+// - ractangular matrices with intermediate values (from 11 to 20) 
+// - ractangular matrices with big values (from 75 to 125) 
+// - ractangular matrices with negative values (from -10 to 0)  
 
 TEST(CriticalMatrixMultiplicationTest, Vectors) {
 
@@ -433,38 +506,6 @@ TEST(CriticalMatrixMultiplicationTest, EvenDimMatrices) {
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-TEST(CriticalMatrixMultiplicationTest, OverflowMatrices) {
-
-    // Randomly generate the dimensions of A
-    int rowsA = generateRandomNumber();
-    int colsA = generateRandomNumber();
-
-    // Create A matrix 
-    std::vector<std::vector<int>> A(rowsA, std::vector<int>(colsA));
-
-    // Fill A
-    fillMatrixRandomly(A, 2e32, 2e32 + 1);
-
-    // Set proper dim for B matrix
-    int rowsB = colsA;
-    int colsB = generateRandomNumber();
-
-    // Create B matrix
-    std::vector<std::vector<int>> B(rowsB, std::vector<int>(colsB));
-
-    // Fill B
-    fillMatrixRandomly(B);
-
-    std::vector<std::vector<int>> C(rowsA, std::vector<int>(colsB, 0));
-
-    multiplyMatrices(A, B, C, rowsA, colsA, colsB);
-
-    std::vector<std::vector<int>> expected(rowsA, std::vector<int>(colsB, 0));
-    multiplyMatricesWithoutErrors(A,B,expected,rowsA,colsA,colsB);
-
-    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
-}
-
 TEST(CriticalMatrixMultiplicationTest, IdentityMatrix) {
 
     // Randomly generate the dimensions of A
@@ -679,32 +720,6 @@ TEST(CriticalMatrixMultiplicationTest, NegativeValuesRectangularMatrices) {
     ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
-/*
-TEST(CriticalMatrixMultiplicationTest, NULLMatrices) {
-
-    // Create A matrix 
-    std::vector<std::vector<int>> A;
-
-    // Set proper dim for B matrix
-    int rowsB = generateRandomNumber();
-    int colsB = generateRandomNumber();
-
-    // Create B matrix
-    std::vector<std::vector<int>> B(rowsB, std::vector<int>(colsB));
-
-    // Fill B
-    fillMatrixRandomly(B);
-
-    std::vector<std::vector<int>> C(0, std::vector<int>(colsB, 0));
-
-    multiplyMatrices(A, B, C, 0, 0, colsB);
-
-    std::vector<std::vector<int>> expected(0, std::vector<int>(colsB, 0));
-    multiplyMatricesWithoutErrors(A,B,expected,0,0,colsB);
-
-    ASSERT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
-}
-*/
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
